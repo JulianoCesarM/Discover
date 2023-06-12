774bbb5c3a74df4e772a8bb810a053ee3c058370
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2023-06-12 15:37:20 -0300

    Created imperativa-declarativa.js

---
+++ programacao_funcional/imperativa-declarativa.js
@@ -0,0 +1,11 @@
+// Imperativo - Faça da seguinte forma
+let number = 2
+
+function square() {
+  return number * number
+}
+
+number = square()
+
+// Declarativo - Oque fazer e não como fazer
+const square = (n) => n * n
